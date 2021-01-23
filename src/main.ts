0080ecaa7819563b7131e849152032be30d3176b
Ashwin Sathian <ashwinsathyan19@gmail.com>
2021-01-23 22:10:04 +0530

    Deployment updates

--- src/main.ts
+++ src/main.ts
@@ -9,6 +9,6 @@ async function bootstrap() {
   });
   app.setGlobalPrefix('api');
   app.use(bodyParser.json({}));
-  await app.listen(3000);
+  await app.listen(process.env.PORT || 8080);
 }
 bootstrap();
